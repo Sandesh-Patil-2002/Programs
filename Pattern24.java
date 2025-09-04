4be7641d0d23be292c3dd03d0ed89ba4aff7618e
Sandesh Patil <sandeshpatil8830@gmail.com>
2025-09-04 10:50:29 +0530

    Add files via upload

---
+++ Pattern24.java
@@ -0,0 +1,20 @@
+/*
+a
+ba
+cba
+dcba
+edcba
+*/
+class Pattern24
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=i;j>=1;j--)
+				System.out.print((char)(j+96));
+			System.out.println();
+		}
+	}
+}
