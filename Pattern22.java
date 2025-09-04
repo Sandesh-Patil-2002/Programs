af4d24546e0aad652f02a9db6a8f05f1718a725e
Sandesh Patil <sandeshpatil8830@gmail.com>
2025-09-04 10:47:12 +0530

    Add files via upload

---
+++ Pattern22.java
@@ -0,0 +1,20 @@
+/*
+e
+dd
+ccc
+bbbb
+aaaaa
+*/
+class Pattern22
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=n;i>=1;i--)
+		{
+			for(int j=i;j<=n;j++)
+				System.out.print((char)(i+96));
+			System.out.println();
+		}
+	}
+}
