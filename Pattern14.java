0f1b7e96f6097e722af1aa9ea390168fc3e880f1
Sandesh Patil <sandeshpatil8830@gmail.com>
2025-09-04 10:32:49 +0530

    Add files via upload

---
+++ Pattern14.java
@@ -0,0 +1,20 @@
+/*
+edcba
+edcba
+edcba
+edcba
+edcba
+*/
+class Pattern14
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=n;i>=1;i--)
+		{
+			for(int j=n;j>=1;j--)
+				System.out.print((char)(j+96));
+			System.out.println();
+		}
+	}
+}
