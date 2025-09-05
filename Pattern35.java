ee41cdb9244eba15059207c280955b204ae60b9e
Sandesh Patil <sandeshpatil8830@gmail.com>
2025-09-05 10:58:25 +0530

    Add files via upload

---
+++ Pattern35.java
@@ -0,0 +1,20 @@
+/*
+abcde
+bcde
+cde
+de
+e
+*/
+class Pattern35
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=i;j<=n;j++)
+				System.out.print((char)(j+96));
+			System.out.println();
+		}
+	}
+}
