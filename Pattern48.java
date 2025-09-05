6512bc295c2eb5efee3f246ad0319fd503fdc029
Sandesh Patil <sandeshpatil8830@gmail.com>
2025-09-05 11:27:06 +0530

    Add files via upload

---
+++ Pattern48.java
@@ -0,0 +1,22 @@
+/*
+    5
+   45
+  345
+ 2345
+12345
+*/
+class Pattern48
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=n;i>=1;i--)
+		{
+			for(int j=i;j>1;j--)
+				System.out.print(" ");
+			for(int k=i;k<=n;k++)
+				System.out.print(k);
+			System.out.println();
+		}		
+	}
+}
