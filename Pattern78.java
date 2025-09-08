2bba0a72d5be5d9a7bee48ebb34a988b715041a2
Sandesh Patil <sandeshpatil8830@gmail.com>
2025-09-08 13:51:21 +0530

    Add files via upload

---
+++ Pattern78.java
@@ -0,0 +1,28 @@
+/*
+				5
+			9	4	
+		12	8	3
+	14	11	7	2
+15	13	10	6	1
+*/
+class Pattern78
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		int count=n;
+		for(int i=n;i>=1;i--)
+		{
+			for(int j=i;j>1;j--)
+				System.out.print("\t");
+			int c=count;
+			for(int j=i;j<=n;j++)
+			{
+				System.out.print(c+"\t");
+				c-=j+1;
+			}
+			System.out.println();
+			count+=i-1;
+		}		
+	}
+}
