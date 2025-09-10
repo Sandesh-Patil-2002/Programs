ab90bebf2352347f6d600d665d34fc1b6b4f410f
Sandesh Patil <sandeshpatil8830@gmail.com>
2025-09-10 11:02:14 +0530

    Add files via upload

---
+++ Pattern91.java
@@ -0,0 +1,31 @@
+/*
+ONLIE 
+MKHD
+JGC 
+FB 
+A
+15	14	12	9	5
+13	11	8	4
+10	7	3
+5	2
+1
+*/
+class Pattern91
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		int count=(n*(n+1))/2;
+		for(int i=1;i<=n;i++)
+		{
+			int c=count;
+			for(int j=i;j<=n;j++)
+			{
+				System.out.print((char)(c+64));
+				c-=j;
+			}
+			System.out.println();
+			count-=i+1;
+		}
+	}
+}
