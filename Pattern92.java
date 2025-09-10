c9c6f7e8662516b31f31149c28de2615e77c9654
Sandesh Patil <sandeshpatil8830@gmail.com>
2025-09-10 11:02:37 +0530

    Add files via upload

---
+++ Pattern92.java
@@ -0,0 +1,33 @@
+/*
+ACFJO
+ BEIN
+  DHM
+   GL 
+    K
+1	3	6	10	15
+	2	5	9	14
+		4	8	13
+			7	12
+				11
+*/
+class Pattern92
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		int count=1;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=i;j>1;j--)
+				System.out.print(" ");
+			int c=count;
+			for(int j=i;j<=n;j++)
+			{
+				System.out.print((char)(c+64));
+				c+=j+1;
+			}
+			System.out.println();
+			count+=i;
+		}
+	}
+}
