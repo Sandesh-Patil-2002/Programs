4feb15c66d1654ca69e23150d9a94ff806c58cf2
Sandesh Patil <sandeshpatil8830@gmail.com>
2025-09-30 23:49:00 +0530

    Add files via upload

---
+++ Pattern111.java
@@ -0,0 +1,23 @@
+/*
+11111
+12222
+12333
+12344
+12345
+*/
+
+class Pattern111
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+				System.out.print(j);
+			for(int j=i;j<=(n-1);j++)
+				System.out.print(i);
+			System.out.println();
+		}
+	}
+}
