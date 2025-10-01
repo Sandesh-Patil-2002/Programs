3d778ddd1de28696518563a44ffc7a5bf85aea01
Sandesh Patil <sandeshpatil8830@gmail.com>
2025-10-01 20:05:35 +0530

    Add files via upload

---
+++ Pattern124.java
@@ -0,0 +1,36 @@
+/*
+    5
+   44
+  333
+ 2222
+11111
+ 2222
+  333
+   44
+    5
+*/
+class Pattern124
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=1;i<=n*2-1;i++)
+		{
+			if(i<=n)
+			{
+				for(int j=i;j<n;j++)
+					System.out.print(" ");
+				for(int j=i;j>=1;j--)
+					System.out.print(n-i+1);
+			}
+			else
+			{
+				for(int j=i;j>n;j--)
+					System.out.print(" ");
+				for(int j=i;j<=n*2-1;j++)
+					System.out.print(i-n+1);
+			}
+			System.out.println();
+		}
+	}
+}
